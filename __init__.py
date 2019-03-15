865e48a2b1fd0ec254078643df58c7399ede230f
CHN\panxi <panxi@westwin.com>
2019-03-15 16:51:56 +0800

    +get deug detail

--- __init__.py
+++ __init__.py
@@ -1,9 +1,18 @@
 from facebooktoken import FacebookTokenRefresher
+
+app_id = "261234567890"
+app_secret="3hniw8u9it5ojkdlforoir"
+app_access_token="EAADvIDZCA94urhxkjfllwijrflksvldijfroer8euut3ie4uhtkedjrnfekrhyeikrfnbemdrjngqoHAZD"
+checked_access_token="EAADvIDZ384ihflerkjmlwudhkefhkshfy74y8347y5i3b4jnbh5tnhhvkiujfie4e7yieuh4LqoHAZD"
 ftr = FacebookTokenRefresher(
-	app_id="1111111111",
-	app_secret="wqtwtrwetweywetytywee",
-	short_access_token="ELAJFLKJNFLJKFKLJFLAJFLAKJKNAFJNKLJNLASKNJLSNF……"
+	app_id=app_id,
+	app_secret=app_secret,
+	short_access_token=app_access_token
 )
 
+#refresh token
 results = ftr.refresh()
+
+#get token detail
+results = ftr.detail(app_access_token,checked_access_token)
 print ('1323423')
--- facebooktoken/base.py
+++ facebooktoken/base.py
@@ -6,10 +6,10 @@ import json
 class FacebookTokenRefresherException(Exception):
     pass
 
-
 class FacebookTokenRefresher:
     base_url = 'https://graph.facebook.com/oauth/access_token'
-    
+    debug_token_url = 'https://graph.facebook.com/debug_token'
+    proxy_ip='http://proxy.******.com:8080'
 
     def __init__(self, app_id, app_secret, short_access_token):
         self.app_id = app_id
@@ -23,13 +23,34 @@ class FacebookTokenRefresher:
             'client_secret': self.app_secret,
             'fb_exchange_token': self.short_access_token
         }
-        proxy_ip='http://proxy.****.*****.com:8080'
-        proxy_setting={'http': proxy_ip,'https': proxy_ip}
-        print (proxy_ip)
+        proxy_setting={'http': self.proxy_ip,'https': self.proxy_ip}
+        print (self.proxy_ip)
         print (proxy_setting)
         print (self.base_url)
         print (params)
         r = requests.get(self.base_url, params=params,proxies=proxy_setting)
+        print (r.url)
+        print (r.text)
+        if r.status_code == 200:
+            return dict(parse.parse_qsl(r.text))
+        else:
+            if raise_exception:
+                raise FacebookTokenRefresherException(json.dumps(r.json(), indent=4))
+            else:
+                return r.json()
+    
+    def detail(self, app_access_token,raise_exception=True):
+        params = {
+            'input_token': app_access_token,
+            'access_token': self.short_access_token
+        }
+        
+        proxy_setting={'http': self.proxy_ip,'https': self.proxy_ip}
+        print (self.proxy_ip)
+        print (proxy_setting)
+        print (self.debug_token_url)
+        print (params)
+        r = requests.get(self.debug_token_url, params=params,proxies=proxy_setting)
         print (r.url)
         print (r.text)
         if r.status_code == 200:
